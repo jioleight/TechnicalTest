c843427b58ce37b420502c6933bf5ea312e41af8
jioleight <jiofreed01@gmail.com>
2018-10-14 20:40:28 +0800

    Comment

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -64,7 +64,7 @@ export default class LoginComponent extends React.Component {
       checked: !state.checked,
     }));
     this.setState((state) => ({
-
+      // Save credentials here
     }));
   }
 
