183531d514db3464c0902f7bec7e6902238fdc0c
jioleight <jiofreed01@gmail.com>
2018-10-19 08:40:56 +0800

    splash

--- App.js
+++ App.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import { createStackNavigator } from 'react-navigation';
 import LoginComponent from './components/LoginComponent';
-/* import SplashComponent from './components/SplashComponent'; */
 import ProfileComponent from './components/ProfileComponent';
 
 const Application = createStackNavigator({
-  /* Splash: { 
-    screen: SplashComponent,
-    navigationOptions: {
-        header: null,
-      },
-  }, */
   Home: { 
     screen: LoginComponent,
     navigationOptions: {
